aabfcdbc558a277586901f7b6c46e95ec9e33bf0
Baran <ozerbar@hotmail.com>
2023-11-23 13:50:28 +0100

    bitwise

--- lab_03/Lab03_DAC.X/src/lab03.c
+++ lab_03/Lab03_DAC.X/src/lab03.c
@@ -63,19 +63,28 @@ void dac_initialize()
     Nop();
 }
 
-void dac_output(uint8_t data[])
+void dac_output(uint16_t voltage)
 {
     CLEARBIT(DAC_CS_PORT);
     Nop();
+    
+    voltage &= 0b0001111111111111;
+    voltage |= 0b0001000000000000;
+    
 
     uint8_t i = 0;
     for(i=0 ; i<16 ; i++)
     {
-        if(data[i]==0)
+        if( (voltage >> (15-i)) %2 == 0)
             CLEARBIT(DAC_SDI_PORT);
         else
             SETBIT(DAC_SDI_PORT);
         Nop();
+//        if(data[i]==0)
+//            CLEARBIT(DAC_SDI_PORT);
+//        else
+//            SETBIT(DAC_SDI_PORT);
+//        Nop();
         CLEARBIT(DAC_SCK_PORT);
         Nop();
         Nop();
@@ -153,23 +162,27 @@ void main_loop()
     CLEARLED(LED1_PORT);
     
     //dac_initialize();
-    uint8_t data1[16] = {0,0,0,1,0,0,1,1,1,1,1,0,1,0,0,0};
-    uint8_t data2[16] = {0,0,0,1,1,0,0,1,1,1,0,0,0,1,0,0};
-    uint8_t data3[16] = {0,0,0,1,1,1,0,0,1,1,1,0,0,1,0,0};
+    //uint8_t data1[16] = {0,0,0,1,0,0,1,1,1,1,1,0,1,0,0,0};
+    //uint8_t data2[16] = {0,0,0,1,1,0,0,1,1,1,0,0,0,1,0,0};
+    //uint8_t data3[16] = {0,0,0,1,1,1,0,0,1,1,1,0,0,1,0,0};
+    uint16_t voltage = 0;
     //dac_output(data);
     while(TRUE)
     {
-        dac_output(data1);
+        voltage = 1000;
+        dac_output(voltage);
         timer_initialize_start(64);
         while(FLAG_WAIT_COMPLETE == 0);
         FLAG_WAIT_COMPLETE = 0;
         
-        dac_output(data2);
+        voltage = 2500;
+        dac_output(voltage);
         timer_initialize_start(256);
         while(FLAG_WAIT_COMPLETE == 0);
         FLAG_WAIT_COMPLETE = 0;
         
-        dac_output(data3);
+        voltage = 3300;
+        dac_output(voltage);
         timer_initialize_start(128);
         while(FLAG_WAIT_COMPLETE == 0);
         FLAG_WAIT_COMPLETE = 0;
